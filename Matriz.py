53eb0a342b5ad48339bfa678e24d9ea650e401a5
Ezequiel Morais <ezequielmorais4002@gmail.com>
2023-03-26 11:53:39 -0300

    Criada classe Matriz com função para gerar Matriz aleatória

---
+++ Matriz.py
@@ -0,0 +1,11 @@
+import random
+class Matriz:
+    @staticmethod
+    def random(linhas, colunas):
+        matriz = []
+        for i in range(linhas):
+            linha = []
+            for j in range(colunas):
+                linha.append(random.randint(0,100))
+            matriz.append(linha)
+        return matriz

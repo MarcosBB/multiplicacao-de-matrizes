86cc147d8dd5c0bf131e30f8b55a7a951685a868
MarcosBB <marcosberaldobarros@hotmail.com>
2023-04-10 17:50:51 -0300

    ♻️ refactor: fixing files structure

--- main.py
+++ main.py
@@ -1,63 +1,33 @@
-from Matriz import Matriz
+from Matriz import matriz_randomica, multi_processo, multi_thread, sequencial
 import time
 import numpy as np
-import os
-import ctypes
-import sys
-import multiprocessing
 
-def calculaElemento(matriz1, matriz2, linha, coluna, aux_num):
-    for el in range(np.shape(matriz2)[1]):
-        aux_num.value += matriz1[linha][el] * matriz2[el][coluna]
+matriz_1 = matriz_randomica(100,100)
+matriz_2 = matriz_randomica(100,100)
 
-def forks(matriz1, matriz2):
-    print("Matriz esperada: \n", np.dot(matriz1, matriz2), "\n")
-    aux_num = multiprocessing.Value('i', 0)
-    resultado = np.zeros((np.shape(matriz1)[0], np.shape(matriz2)[1]), dtype=int)
-    if (np.shape(matriz1)[0] != np.shape(matriz2)[1]):
-        print("Não é possível multiplicar as duas matrizes")
-    else:
-        comeco = time.time()
-        for linha in range(0,np.shape(matriz1)[0]):
-            for coluna in range(0,np.shape(matriz2)[1]):
-                filho = multiprocessing.Process(target=calculaElemento, args=(matriz1,matriz2,linha,coluna,aux_num))
-                filho.start()
-                resultado[linha][coluna] = aux_num.value
-                aux_num.value = 0
-        fim = time.time()
-        tempo_decorrido = float((fim - comeco))
-        print("Matriz Resultante: \n", resultado)
-        print("\n A multiplicação levou um total de ", tempo_decorrido, " segundos.")
-        return tempo_decorrido
-    
-def sequencial(matriz1, matriz2):
-    print("Matriz esperada: \n", np.dot(matriz1,matriz2), "\n")
-    aux_num = 0
-    num_elementos = 0
-    resultado = np.zeros((np.shape(matriz1)[0], np.shape(matriz2)[1]), dtype=int)
-    if(np.shape(matriz1)[0] != np.shape(matriz2)[1]):
-        print("Não é possível multiplicar as duas matrizes")
-    else:
-        comeco = time.time()
-        for linha in range(np.shape(matriz1)[0]):
-            for coluna in range(np.shape(matriz2)[1]):
-                num_elementos += 1
-                for el in range(np.shape(matriz2)[1]):
-                    aux_num += matriz1[linha][el] * matriz2[el][coluna]
-                resultado[linha][coluna] = aux_num
-                aux_num = 0
-        fim = time.time()
-        tempo_decorrido = float((fim - comeco))
-        print("Matriz Resultante: \n", resultado)
-        print("\n A multiplicação levou um total de ", tempo_decorrido, " segundos.")
-        return tempo_decorrido
+if (np.shape(matriz_1)[0] != np.shape(matriz_2)[1]):
+    print("Não é possível multiplicar as duas matrizes")
 
+else:
+    shape = np.zeros((np.shape(matriz_1)[0], np.shape(matriz_2)[1]), dtype=int)
 
-matriz_1 = Matriz.random(100,100)
-matriz_2 = Matriz.random(100,100)
-print("IMPLEMENTAÇÃO SEQUENCIAL: \n")
-sequencial(matriz_1, matriz_2)
-print("__________________________________________________________________________\n")
-print("IMPLEMENTAÇÃO COM CRIAÇÃO DE PROCESSOS: \n")
-forks(matriz_1, matriz_2)
-print("__________________________________________________________________________\n")
+    print("IMPLEMENTAÇÃO SEQUENCIAL: \n")
+    comeco = time.time()
+    resultado = sequencial(matriz_1, matriz_2, shape)
+    fim = time.time()
+    print("\n A multiplicação levou um total de ", float((fim - comeco)), " segundos.")
+    print("__________________________________________________________________________\n")
+
+    print("IMPLEMENTAÇÃO COM CRIAÇÃO DE PROCESSOS: \n")
+    comeco = time.time()
+    resultado = multi_processo(matriz_1, matriz_2, shape)
+    fim = time.time()
+    print("\n A multiplicação levou um total de ", float((fim - comeco)), " segundos.")
+    print("__________________________________________________________________________\n")
+
+    print("IMPLEMENTAÇÃO COM THREADS: \n")
+    comeco = time.time()
+    resultado = multi_thread(matriz_1, matriz_2, shape)
+    fim = time.time()
+    print("\n A multiplicação levou um total de ", float((fim - comeco)), " segundos.")
+    print("__________________________________________________________________________\n")
